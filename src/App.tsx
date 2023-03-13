ad6f61c7530004f935376d7f3de08dc60ac925f6
Roman Pogorelov <ramzes.terentiy@gmail.com>
2023-03-13 11:58:24 +0100

    add form

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import AddCompany from './components/AddCompany'
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="contentWrapper">
+        <AddCompany />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AddCompany.tsx
@@ -0,0 +1,91 @@
+import { Button, Form, Input, Typography } from "antd";
+import { FC, useState } from "react";
+
+const AddCompany: FC = () => {
+  const [submitButtonState, setSubmitButtonState] = useState<{
+    text: string;
+    disabled: boolean;
+  }>({
+    text: "Create",
+    disabled: false,
+  });
+  const [resetButtonState, setResetButtonState] = useState<{
+    text: string;
+    disabled: boolean;
+  }>({
+    text: "Reset",
+    disabled: false,
+  });
+
+  const [formState, setFormState] = useState<{
+    name: string;
+    domain: string;
+  }>({
+    name: "Example company name",
+    domain: "domain.com",
+  });
+
+  const onSubmitClick = () => {
+    console.log(formState);
+    setSubmitButtonState({ text: "Success", disabled: true });
+  };
+  const onResetClick = () => {
+    setFormState({
+      name: "",
+      domain: "",
+    });
+    setSubmitButtonState({ text: "Create", disabled: false });
+  };
+
+  return (
+    <>
+      <Typography.Title level={3}>Add company</Typography.Title>
+      <Form
+        labelCol={{ span: 6 }}
+        name="basic"
+        layout="horizontal"
+        initialValues={formState}
+        autoComplete="off"
+        style={{ width: "30%", margin: "auto" }}
+      >
+        <Form.Item label="Name" name="name">
+          <Input
+            value={formState.name}
+            onChange={(value) =>
+              setFormState({ ...formState, name: value.target.value })
+            }
+          />
+        </Form.Item>
+        <Form.Item label="Domain" name="domain">
+          <Input
+            value={formState.domain}
+            onChange={(value) =>
+              setFormState({ ...formState, domain: value.target.value })
+            }
+          />
+        </Form.Item>
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="button"
+            disabled={submitButtonState.disabled}
+            onClick={onSubmitClick}
+          >
+            {submitButtonState.text}
+          </Button>
+        </Form.Item>
+        <Form.Item>
+          <Button
+            type="dashed"
+            htmlType="button"
+            disabled={resetButtonState.disabled}
+            onClick={onResetClick}
+          >
+            {resetButtonState.text}
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
+};
+export default AddCompany;
