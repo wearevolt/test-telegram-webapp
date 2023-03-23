0ac9a38c9f9d09cddce4b6080649cd080e396cbd
Roman Pogorelov <ramzes.terentiy@gmail.com>
2023-03-23 14:03:15 +0100

    work

--- src/queryes/index.ts
+++ src/queryes/index.ts
@@ -2,8 +2,8 @@ import { useQuery, gql } from "@apollo/client";
 import { TgInitData, TgMessageFilter } from "../types";
 
 const getTokenQuery = gql`
-  mutation AuthWebapp($initStr: String!, $deviceId: String!, $initData: TgInitData) {
-    authWebapp(init_str: $initStr, device_id: $deviceId, init_data: $initData) {
+  query AuthWebapp($initStr: String!, $deviceId: String!, $initData: TgInitData) {
+    getToken(init_str: $initStr, device_id: $deviceId, init_data: $initData) {
       jwtToken
     }
   }
